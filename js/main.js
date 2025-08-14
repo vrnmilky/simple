33d9664c20e6e26f839e8fe0b0b067cc8bf9d112
Borodin Pavel <vrn.milky@yandex.ru>
2025-08-14 11:40:38 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const body = document.querySelector("body")
+const burger = document.querySelector('.header_burger')
+const navMenu = document.querySelector('.header_nav')
+
+const search = document.querySelector('.header_actions__btn')
+const closeSearch = document.querySelector('.header_actions__close')
+const containerInfo = document.querySelector('.header_container')
+const searchInput = document.querySelector('#searchInput')
+
+
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active')
+  navMenu.classList.toggle('active')
+  body.classList.toggle('no-scroll')
+})
+
+search.addEventListener('click', () => {
+  if (!containerInfo.classList.contains('search')) {
+    containerInfo.classList.add('search')
+  }
+})
+
+closeSearch.addEventListener('click', () => {
+  if (containerInfo.classList.contains('search')) {
+    containerInfo.classList.remove('search')
+    searchInput.value = ''
+  }
+})
