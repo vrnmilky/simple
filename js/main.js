89f5756a8bac3fe2186526ffcf4ed9f96d1facf1
Borodin Pavel <vrn.milky@yandex.ru>
2025-08-14 15:59:12 +0300

    fix: component states

--- js/main.js
+++ js/main.js
@@ -13,17 +13,20 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('active')
   navMenu.classList.toggle('active')
   body.classList.toggle('no-scroll')
+
 })
 
 search.addEventListener('click', () => {
   if (!containerInfo.classList.contains('search')) {
     containerInfo.classList.add('search')
+    search.classList.add('active')
   }
 })
 
 closeSearch.addEventListener('click', () => {
   if (containerInfo.classList.contains('search')) {
     containerInfo.classList.remove('search')
+    search.classList.remove('active')
     searchInput.value = ''
   }
 })
